418b9c03949bdc835ccafbb42e38ec79741b769b
Jeandré Rohlandt <4688503+jrohlandt@users.noreply.github.com>
2026-01-08 18:58:09 +0200

    Markup, sample data, basic styling.

--- main.js
+++ main.js
@@ -1,3 +1,83 @@
-import data from "./data/sample-data.json" with {type: "json"};
+// import data from "./data/sample-data.json" with {type: "json"};
+import data from "./data/main-data.json" with {type: "json"};
 
-console.log(data.something);
+console.log(data);
+
+// NAME
+const nameEl = document.getElementById('name');
+nameEl.innerHTML = data.name;
+
+// LOCATION
+const locationEl = document.getElementById('location');
+locationEl.innerHTML = data.location;
+
+// CONTACT
+const contactEl = document.getElementById('contact');
+contactEl.innerHTML = data.contact;
+
+// SUMMARY
+const summaryEl = document.getElementById('summary');
+summaryEl.innerHTML = data.summary;
+
+// SKILLS
+const skillsEl = document.getElementById('skills');
+let skillsText = "";
+data.skills.forEach((line) => {
+    skillsText += line + '<br>';
+});
+skillsEl.innerHTML = skillsText;
+
+// WORK EXPERIENCE
+const workEl = document.getElementById('work');
+const createWorkExperienceCard = (exp, bottomSpacing=false) => {
+    
+    let card = `
+    <div class="work-experience-card ${bottomSpacing ? 'space-bottom' : ''}"> 
+        <div>
+            <a href="${exp.companyUrl}" target="_blank">${exp.companyName}</a> 
+            <span>${exp.duration}</span>
+        </div>
+        <div>
+            <div>${exp.role}</div>
+            <div>${exp.locationType}</div>
+        </div>
+        <ul>
+    `;
+
+    exp.bulletPoints.forEach(b => {
+        card += `<li>${b}</li>`;
+    });
+
+    card += `
+        </ul>
+    </div>
+    `;
+
+    const el = document.createElement('li');
+    el.innerHTML = `<li>${card}</li>`;
+    workEl.appendChild(el);
+}
+
+
+data.workExperience.forEach((exp, i, arr) => {
+    createWorkExperienceCard(exp, i !== arr.length - 1);
+});
+
+// EDUCATION
+const educationEl = document.getElementById('education');
+const createEducationCard = (edu, bottomSpacing=false) => {
+    let card = `
+        <div>
+            ${edu.instituteName} ${edu.qualification}
+        </div>
+    `;
+
+    const el = document.createElement('li');
+    el.innerHTML = `<li>${card}</li>`;
+    educationEl.appendChild(el);
+};
+
+data.education.forEach((edu, i, arr) => {
+    createEducationCard(edu, i !== arr.length - 1);
+});
+
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 Usage:
 
-1. Serve with a local webserver. E.g. python -m http.server 3000
-2. Open with a web browser like Chrome and then use the print feature to save as PDF.
+1. Copy and rename /data/sample-data.json and replace data with your own.
+2. Serve with a local webserver. E.g. python -m http.server 3000
+3. Open with a web browser like Chrome and then use the print feature to save as PDF.
